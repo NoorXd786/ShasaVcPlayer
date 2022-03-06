80bd562b04b361f0fc695757fd45bfaaa76cb3d2
MdNoor786 <MdNoor786@users.noreply.github.com>
2022-03-06 11:10:17 +0000

    pylint: auto fixes

--- ShasaMusic/plugins/sudo/private.py
+++ ShasaMusic/plugins/sudo/private.py
@@ -11,14 +11,16 @@ from pyrogram import filters
 from pyrogram.types import Message
 
 import config
-from strings import get_command
 from ShasaMusic import app
 from ShasaMusic.misc import SUDOERS
-from ShasaMusic.utils.database import (add_private_chat,
-                                       get_private_served_chats,
-                                       is_served_private_chat,
-                                       remove_private_chat)
+from ShasaMusic.utils.database import (
+    add_private_chat,
+    get_private_served_chats,
+    is_served_private_chat,
+    remove_private_chat,
+)
 from ShasaMusic.utils.decorators.language import language
+from strings import get_command
 
 AUTHORIZE_COMMAND = get_command("AUTHORIZE_COMMAND")
 UNAUTHORIZE_COMMAND = get_command("UNAUTHORIZE_COMMAND")
