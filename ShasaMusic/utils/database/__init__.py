2569ca4929a576777d33ed29ad63e1f5731f690b
Md Noor <75603153+Mdnoor786@users.noreply.github.com>
2022-03-06 16:31:50 +0530

    Add files via upload

---
+++ ShasaMusic/utils/database/__init__.py
@@ -0,0 +1,12 @@
+#
+# Copyright (C) 2021-2022 by MdNoor786@Github, < https://github.com/MdNoor786 >.
+#
+# This file is part of < https://github.com/MdNoor786/ShasaVcPlayer > project,
+# and is released under the "GNU v3.0 License Agreement".
+# Please see < https://github.com/MdNoor786/ShasaVcPlayer/blob/master/LICENSE >
+#
+# All rights reserved.
+
+from .assistantdatabase import *
+from .memorydatabase import *
+from .mongodatabase import *
