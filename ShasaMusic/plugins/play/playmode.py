2164b595b5cda98e717ed5a7cd7b5c1ae90daa35
Md Noor <75603153+Mdnoor786@users.noreply.github.com>
2022-03-06 16:39:13 +0530

    Add files via upload

---
+++ ShasaMusic/plugins/play/playmode.py
@@ -0,0 +1,49 @@
+#
+# Copyright (C) 2021-2022 by MdNoor786@Github, < https://github.com/MdNoor786 >.
+#
+# This file is part of < https://github.com/MdNoor786/ShasaVcPlayer > project,
+# and is released under the "GNU v3.0 License Agreement".
+# Please see < https://github.com/MdNoor786/ShasaVcPlayer/blob/master/LICENSE >
+#
+# All rights reserved.
+
+from pyrogram import filters
+from pyrogram.types import InlineKeyboardMarkup, Message
+
+from config import BANNED_USERS
+from strings import get_command
+from ShasaMusic import app
+from ShasaMusic.utils.database import (get_chatmode, get_playmode,
+                                       get_playtype)
+from ShasaMusic.utils.decorators import language
+from ShasaMusic.utils.inline.settings import playmode_users_markup
+
+### Commands
+PLAYMODE_COMMAND = get_command("PLAYMODE_COMMAND")
+
+
+@app.on_message(
+    filters.command(PLAYMODE_COMMAND) & filters.group & ~BANNED_USERS
+)
+@language
+async def playmode_(client, message: Message, _):
+    playmode = await get_playmode(message.chat.id)
+    if playmode == "Direct":
+        Direct = True
+    else:
+        Direct = None
+    chatmode = await get_chatmode(message.chat.id)
+    if chatmode == "Group":
+        Group = True
+    else:
+        Group = None
+    playty = await get_playtype(message.chat.id)
+    if playty == "Everyone":
+        Playtype = None
+    else:
+        Playtype = True
+    buttons = playmode_users_markup(_, Direct, Group, Playtype)
+    response = await message.reply_text(
+        _["playmode_1"].format(message.chat.title),
+        reply_markup=InlineKeyboardMarkup(buttons),
+    )
---
+++ ShasaMusic/plugins/play/stream.py
@@ -0,0 +1,82 @@
+#
+# Copyright (C) 2021-2022 by MdNoor786@Github, < https://github.com/MdNoor786 >.
+#
+# This file is part of < https://github.com/MdNoor786/ShasaVcPlayer > project,
+# and is released under the "GNU v3.0 License Agreement".
+# Please see < https://github.com/MdNoor786/ShasaVcPlayer/blob/master/LICENSE >
+#
+# All rights reserved.
+
+from pyrogram import filters
+from pyrogram.types import Message
+from pytgcalls.exceptions import NoActiveGroupCall
+
+import config
+from config import BANNED_USERS
+from strings import get_command
+from ShasaMusic import app
+from ShasaMusic.core.call import Shasa
+from ShasaMusic.utils.decorators.play import PlayWrapper
+from ShasaMusic.utils.logger import play_logs
+from ShasaMusic.utils.stream.stream import stream
+
+# Command
+STREAM_COMMAND = get_command("STREAM_COMMAND")
+
+
+@app.on_message(
+    filters.command(STREAM_COMMAND) & filters.group & ~BANNED_USERS
+)
+@PlayWrapper
+async def stream_command(
+    client,
+    message: Message,
+    _,
+    chat_id,
+    video,
+    channel,
+    playmode,
+    mystic,
+    url,
+):
+    if url:
+        try:
+            await Shasa.stream_call(url)
+        except NoActiveGroupCall:
+            await mystic.edit_text(
+                "There's an issue with the bot. Please report it to my owner and ask them to check logger group."
+            )
+            return await app.send_message(
+                config.LOG_GROUP_ID,
+                "Please turn on Voice Chat.. Bot is not able to stream urls..",
+            )
+        except Exception as e:
+            return await mystic.edit_text(
+                _["general_3"].format(type(e).__name__)
+            )
+        await mystic.edit_text(_["str_2"])
+        try:
+            await stream(
+                _,
+                mystic,
+                message.from_user.id,
+                url,
+                chat_id,
+                message.from_user.first_name,
+                message.chat.id,
+                video=True,
+                streamtype="index",
+            )
+        except Exception as e:
+            ex_type = type(e).__name__
+            err = (
+                e
+                if ex_type == "AssistantErr"
+                else _["general_3"].format(ex_type)
+            )
+            return await mystic.edit_text(err)
+        return await play_logs(
+            message, streamtype="M3u8 or Index Link"
+        )
+    else:
+        await mystic.edit_text(_["str_1"])
