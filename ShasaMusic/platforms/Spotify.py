78cfec6e70b79daa662f1c672721f6d7d725e178
MdNoor786 <MdNoor786@users.noreply.github.com>
2022-03-07 04:44:42 +0000

    pylint: auto fixes

--- ShasaMusic/platforms/Spotify.py
+++ ShasaMusic/platforms/Spotify.py
@@ -22,10 +22,8 @@ class SpotifyAPI:
         self.client_id = config.SPOTIFY_CLIENT_ID
         self.client_secret = config.SPOTIFY_CLIENT_SECRET
         if config.SPOTIFY_CLIENT_ID and config.SPOTIFY_CLIENT_SECRET:
-            self.client_credentials_manager = (
-                SpotifyClientCredentials(
-                    self.client_id, self.client_secret
-                )
+            self.client_credentials_manager = SpotifyClientCredentials(
+                self.client_id, self.client_secret
             )
             self.spotify = spotipy.Spotify(
                 client_credentials_manager=self.client_credentials_manager
@@ -73,7 +71,5 @@ class SpotifyAPI:
 
     async def trackplaylist(self, trackid):
         meta = self.spotify.track(trackid)
-        to_search = (
-            f"{meta['name']} {meta['album']['artists'][0]['name']}"
-        )
+        to_search = f"{meta['name']} {meta['album']['artists'][0]['name']}"
         return to_search
