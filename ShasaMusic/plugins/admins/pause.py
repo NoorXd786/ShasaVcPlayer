3bf0e2a4567da7806088c40ef24ffbf5edaca867
Md Noor <75603153+Mdnoor786@users.noreply.github.com>
2022-03-07 10:15:32 +0530

    Add files via upload

--- ShasaMusic/plugins/admins/pause.py
+++ ShasaMusic/plugins/admins/pause.py
@@ -1,9 +1,9 @@
 #
-# Copyright (C) 2021-2022 by MdNoor786@Github, < https://github.com/MdNoor786 >.
+# Copyright (C) 2021-2022 by MdNoor@Github, < https://github.com/MdNoor786 >.
 #
 # This file is part of < https://github.com/MdNoor786/ShasaVcPlayer > project,
 # and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/MdNoor786/ShasaVcPlayer/blob/master/LICENSE >
+# Please see < https://github.com/MdNoor786/ShasaVcPlayer/blob/main/LICENSE >
 #
 # All rights reserved.
 
@@ -11,23 +11,27 @@ from pyrogram import filters
 from pyrogram.types import Message
 
 from config import BANNED_USERS
+from strings import get_command
 from ShasaMusic import app
 from ShasaMusic.core.call import Shasa
 from ShasaMusic.utils.database import is_music_playing, music_off
 from ShasaMusic.utils.decorators import AdminRightsCheck
-from strings import get_command
 
 # Commands
 PAUSE_COMMAND = get_command("PAUSE_COMMAND")
 
 
-@app.on_message(filters.command(PAUSE_COMMAND) & filters.group & ~BANNED_USERS)
+@app.on_message(
+    filters.command(PAUSE_COMMAND) & filters.group & ~BANNED_USERS
+)
 @AdminRightsCheck
-async def pause_admin(cli, message: Message, _, mystic, chat_id):
+async def pause_admin(cli, message: Message, _, chat_id):
     if not len(message.command) == 1:
-        return await mystic.edit_text(_["general_2"])
+        return await message.reply_text(_["general_2"])
     if not await is_music_playing(chat_id):
-        return await mystic.edit_text(_["admin_1"])
+        return await message.reply_text(_["admin_1"])
     await music_off(chat_id)
     await Shasa.pause_stream(chat_id)
-    await mystic.edit_text(_["admin_2"].format(message.from_user.mention))
+    await message.reply_text(
+        _["admin_2"].format(message.from_user.mention)
+    )
--- ShasaMusic/plugins/admins/shuffle.py
+++ ShasaMusic/plugins/admins/shuffle.py
@@ -1,9 +1,9 @@
 #
-# Copyright (C) 2021-2022 by MdNoor786@Github, < https://github.com/MdNoor786 >.
+# Copyright (C) 2021-2022 by MdNoor@Github, < https://github.com/MdNoor786 >.
 #
 # This file is part of < https://github.com/MdNoor786/ShasaVcPlayer > project,
 # and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/MdNoor786/ShasaVcPlayer/blob/master/LICENSE >
+# Please see < https://github.com/MdNoor786/ShasaVcPlayer/blob/main/LICENSE >
 #
 # All rights reserved.
 
@@ -13,32 +13,35 @@ from pyrogram import filters
 from pyrogram.types import Message
 
 from config import BANNED_USERS
+from strings import get_command
 from ShasaMusic import app
 from ShasaMusic.misc import db
 from ShasaMusic.utils.decorators import AdminRightsCheck
-from strings import get_command
 
 # Commands
 SHUFFLE_COMMAND = get_command("SHUFFLE_COMMAND")
 
 
-@app.on_message(filters.command(SHUFFLE_COMMAND) & filters.group & ~BANNED_USERS)
+@app.on_message(
+    filters.command(SHUFFLE_COMMAND) & filters.group & ~BANNED_USERS
+)
 @AdminRightsCheck
-async def admins(Client, message: Message, _, mystic, chat_id):
+async def admins(Client, message: Message, _, chat_id):
     if not len(message.command) == 1:
-        return await mystic.edit_text(_["general_2"])
+        return await message.reply_text(_["general_2"])
     check = db.get(chat_id)
     if not check:
-        return await mystic.edit_text(_["admin_21"])
-    print(len(check))
+        return await message.reply_text(_["admin_21"])
     try:
         popped = check.pop(0)
     except:
-        return await mystic.edit_text(_["admin_22"])
+        return await message.reply_text(_["admin_22"])
     check = db.get(chat_id)
     if not check:
         check.insert(0, popped)
-        return await mystic.edit_text(_["admin_22"])
+        return await message.reply_text(_["admin_22"])
     random.shuffle(check)
     check.insert(0, popped)
-    await mystic.edit_text(_["admin_23"].format(message.from_user.first_name))
+    await message.reply_text(
+        _["admin_23"].format(message.from_user.first_name)
+    )
