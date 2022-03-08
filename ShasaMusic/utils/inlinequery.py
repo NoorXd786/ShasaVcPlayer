4517c4c8bf47685ed2697b315517e0c01689de3e
Md Noor <75603153+Mdnoor786@users.noreply.github.com>
2022-03-08 10:15:31 +0530

    Add files via upload

--- ShasaMusic/utils/inlinequery.py
+++ ShasaMusic/utils/inlinequery.py
@@ -1,13 +1,14 @@
 #
-# Copyright (C) 2021-2022 by MdNoor@Github, < https://github.com/MdNoor786 >.
+# Copyright (C) 2021-2022 by MdNoor786@Github, < https://github.com/MdNoor786 >.
 #
 # This file is part of < https://github.com/MdNoor786/ShasaVcPlayer > project,
 # and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/MdNoor786/ShasaVcPlayer/blob/main/LICENSE >
+# Please see < https://github.com/MdNoor786/ShasaVcPlayer/blob/master/LICENSE >
 #
 # All rights reserved.
 
-from pyrogram.types import InlineQueryResultArticle, InputTextMessageContent
+from pyrogram.types import (InlineQueryResultArticle,
+                            InputTextMessageContent)
 
 answer = []
 
