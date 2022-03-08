953ee291dc5daf1c812bf24e39c0e0847772aa02
MdNoor786 <MdNoor786@users.noreply.github.com>
2022-03-08 04:50:23 +0000

    pylint: auto fixes

--- ShasaMusic/plugins/sudo/globalban.py
+++ ShasaMusic/plugins/sudo/globalban.py
@@ -7,24 +7,25 @@
 #
 # All rights reserved.
 import asyncio
-import time
 
 from pyrogram import filters
 from pyrogram.errors import FloodWait
 from pyrogram.types import Message
 
 from config import BANNED_USERS
-from strings import get_command
 from ShasaMusic import app
 from ShasaMusic.misc import SUDOERS
 from ShasaMusic.utils import get_readable_time
-from ShasaMusic.utils.database import (add_banned_user,
-                                       get_banned_count,
-                                       get_banned_users,
-                                       get_served_chats,
-                                       is_banned_user,
-                                       remove_banned_user)
+from ShasaMusic.utils.database import (
+    add_banned_user,
+    get_banned_count,
+    get_banned_users,
+    get_served_chats,
+    is_banned_user,
+    remove_banned_user,
+)
 from ShasaMusic.utils.decorators.language import language
+from strings import get_command
 
 # Command
 GBAN_COMMAND = get_command("GBAN_COMMAND")
@@ -63,9 +64,7 @@ async def gbanuser(client, message: Message, _):
         served_chats.append(int(chat["chat_id"]))
     time_expected = len(served_chats)
     time_expected = get_readable_time(time_expected)
-    mystic = await message.reply_text(
-        _["gban_5"].format(mention, time_expected)
-    )
+    mystic = await message.reply_text(_["gban_5"].format(mention, time_expected))
     number_of_chats = 0
     for chat_id in served_chats:
         try:
@@ -75,9 +74,7 @@ async def gbanuser(client, message: Message, _):
             await asyncio.sleep(int(e.x))
         except Exception:
             pass
-    await message.reply_text(
-        _["gban_6"].format(mention, number_of_chats)
-    )
+    await message.reply_text(_["gban_6"].format(mention, number_of_chats))
     await mystic.delete()
 
 
@@ -106,9 +103,7 @@ async def gungabn(client, message: Message, _):
         served_chats.append(int(chat["chat_id"]))
     time_expected = len(served_chats)
     time_expected = get_readable_time(time_expected)
-    mystic = await message.reply_text(
-        _["gban_8"].format(mention, time_expected)
-    )
+    mystic = await message.reply_text(_["gban_8"].format(mention, time_expected))
     number_of_chats = 0
     for chat_id in served_chats:
         try:
@@ -118,9 +113,7 @@ async def gungabn(client, message: Message, _):
             await asyncio.sleep(int(e.x))
         except Exception:
             pass
-    await message.reply_text(
-        _["gban_9"].format(mention, number_of_chats)
-    )
+    await message.reply_text(_["gban_9"].format(mention, number_of_chats))
     await mystic.delete()
 
 
@@ -138,9 +131,7 @@ async def gbanned_list(client, message: Message, _):
         count += 1
         try:
             user = await app.get_users(user_id)
-            user = (
-                user.first_name if not user.mention else user.mention
-            )
+            user = user.first_name if not user.mention else user.mention
             msg += f"{count}➤ {user}\n"
         except Exception:
             msg += f"{count}➤ [Unfetched User]{user_id}\n"
